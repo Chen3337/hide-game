dd01668d79e830fc3971d61ea521ac8046715b2d
zhen yong chen <dannychen111@yahoo.com>
2020-03-23 00:10:06 -0500

    link change

--- src/components/manu.js
+++ src/components/manu.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 function Manu () {
     return (
         <div style={{width: window.innerWidth, height: window.innerHeight, backgroundColor: 'black'}}>
             <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-                <h1><a href="/hide-game/game/">Start</a></h1>
+                <h1><Link className="nav-link" to={"/hide-game/game/"}>START</Link></h1>
             </div>
         </div>
     )
