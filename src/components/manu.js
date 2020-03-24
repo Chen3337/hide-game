9a79638dc10fde15714cf214030c90199fbb4e7e
zhen yong chen <dannychen111@yahoo.com>
2020-03-23 20:04:25 -0500

    last update to game

--- src/components/manu.js
+++ src/components/manu.js
@@ -3,8 +3,13 @@ import { Link } from "react-router-dom";
 function Manu () {
     return (
         <div style={{width: window.innerWidth, height: window.innerHeight, backgroundColor: 'black'}}>
-            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-                <h1><Link className="nav-link" to={"/hide-game/game/"}>START</Link></h1>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh',width: "80%", margin: "auto", textAlign:"center"}}>
+                <h1><Link style={{ textDecoration: 'none', backgroundColor: 'white'}} to={"/hide-game/game/"}>START</Link>
+                <br />
+                <b style={{color: 'white',fontSize:"16px"}}>There is two cirle, the red one is the main charater and the circle below it is the where you control it the main charater. 
+                    hide from the bullets and score is on the bottom.
+                </b>
+                </h1>
             </div>
         </div>
     )
